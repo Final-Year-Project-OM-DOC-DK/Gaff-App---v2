16ef7c052903a918c8a14f260729859e90e769ee
ossianmoore <g00351479@gmit.ie>
2020-04-19 20:08:02 +0100

    Completed shopping list functionality - Able to add and delete, synced across all members. CSS added

--- src/app/shoppinglist/shoppinglist.page.ts
+++ src/app/shoppinglist/shoppinglist.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {NavController} from "@ionic/angular";
+import {NavController, AlertController} from "@ionic/angular";
+import { AngularFirestore } from '@angular/fire/firestore';
+import { UserService } from '../Services/user.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-shoppinglist',
   templateUrl: './shoppinglist.page.html',
@@ -7,13 +10,79 @@ import {NavController} from "@ionic/angular";
 })
 export class ShoppinglistPage implements OnInit {
 
-  
+  DB;
+  shoppingArray = [];
+  currentHouseId;
 
-    constructor(private navCtrl: NavController) { }
+  shoppingList = {
+    item: '',
+    completed: false
+  }
+
+    constructor(private navCtrl: NavController,
+                private afs: AngularFirestore,
+                private userService: UserService,
+                private alertCtrl: AlertController,
+                private router: Router) { 
+
+      //get ID of house from URL
+      let id1 = this.router.url.split('id=');
+      let id2 = id1[1].toString();
+      let id3 = id2.split('/');
+      let id = id3[0].toString();
+      this.currentHouseId = id;
+      console.log(id);
+
+      //intiialise DB
+      this.DB = this.afs.collection('house').doc(id).collection('shoppingList');
+      //subscribe to shopping list data
+      this.DB.snapshotChanges().subscribe(colSnap => {
+        this.shoppingArray = [];
+        colSnap.forEach(snap => {
+          let item: any = snap.payload.doc.data();
+          item.id = snap.payload.doc.id;
+          this.shoppingArray.push(item);
+          console.log(item);
+        });
+      });
+    }
   
     ngOnInit() {
     }
-    gotoAddToList(){
-      this.navCtrl.navigateForward(['/add-tolist']);
+
+    addItem(){
+      this.DB.add(this.shoppingList);
+      this.resetItem();
     }
+
+    //function to delete message from DB
+    async deleteItem(item){
+    const alert = await this.alertCtrl.create({
+      message: 'Are you sure you want to delete this Item?',
+      buttons: [
+        {
+          text: 'No',
+          handler: () => {
+            return;
+          }
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            this.DB.doc(item).delete()
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  //rest item to default values
+    resetItem(){
+      this.shoppingList = {
+        item: '',
+        completed: false
+      }
+    }
+  
   }
