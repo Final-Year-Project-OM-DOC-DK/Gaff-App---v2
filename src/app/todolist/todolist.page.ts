f8e0243223469163dc29b6ace954377f684eb817
DylanKeane <43648552+DylanKeane@users.noreply.github.com>
2020-04-20 11:02:12 +0100

    todofrontendbackendworking

--- src/app/todolist/todolist.page.ts
+++ src/app/todolist/todolist.page.ts
@@ -1,17 +1,88 @@
 import { Component, OnInit } from '@angular/core';
-import {NavController} from "@ionic/angular";
+import {NavController,AlertController} from "@ionic/angular";
+import { AngularFirestore } from '@angular/fire/firestore';
+import { UserService } from '../Services/user.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-todolist',
   templateUrl: './todolist.page.html',
   styleUrls: ['./todolist.page.scss'],
 })
 export class TodolistPage implements OnInit {
+  DB;
+  todoArray = [];
+  currentHouseId;
 
-  constructor(private navCtrl: NavController) { }
+  todoList = {
+    item: '',
+    completed: false
+  }
 
-  ngOnInit() {
+    constructor(private navCtrl: NavController,
+                private afs: AngularFirestore,
+                private userService: UserService,
+                private alertCtrl: AlertController,
+                private router: Router) { 
+
+      //get ID of house from URL
+      let id1 = this.router.url.split('id=');
+      let id2 = id1[1].toString();
+      let id3 = id2.split('/');
+      let id = id3[0].toString();
+      this.currentHouseId = id;
+      console.log(id);
+
+      //intiialise DB
+      this.DB = this.afs.collection('house').doc(id).collection('todoList');
+      //subscribe to shopping list data
+      this.DB.snapshotChanges().subscribe(colSnap => {
+        this.todoArray = [];
+        colSnap.forEach(snap => {
+          let item: any = snap.payload.doc.data();
+          item.id = snap.payload.doc.id;
+          this.todoArray.push(item);
+          console.log(item);
+        });
+      });
+    }
+  
+    ngOnInit() {
+    }
+
+    addItem(){
+      this.DB.add(this.todoList);
+      this.resetItem();
+    }
+
+    //function to delete message from DB
+    async deleteItem(item){
+    const alert = await this.alertCtrl.create({
+      message: 'Are you sure you want to delete this Item?',
+      buttons: [
+        {
+          text: 'No',
+          handler: () => {
+            return;
+          }
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            this.DB.doc(item).delete()
+          }
+        }
+      ]
+    });
+    await alert.present();
   }
-  gotoAddToDo(){
-    this.navCtrl.navigateForward(['/add-todo']);
+
+  //rest item to default values
+    resetItem(){
+      this.todoList = {
+        item: '',
+        completed: false
+      }
+    }
+  
   }
-}
+  
